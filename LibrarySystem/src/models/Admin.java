7ea2d31ad4cae0ea9d030be6056358277236736b
Mazen Mohamed <mazenkassem101@gmail.com>
2025-12-10 14:37:14 +0200

    Bruh

--- LibrarySystem/src/models/Admin.java
+++ LibrarySystem/src/models/Admin.java
@@ -1,13 +1,113 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
-/**
- *
- * @author El-Abiad
- */
-public class Admin {
+import main.LibrarySystem;
+import java.util.*;
+
+public class Admin extends User {
+    private final LibrarySystem librarySystem;
+
+    public Admin(String username, String password, String phone, String email, LibrarySystem librarySystem) {
+        super(librarySystem.getUserCount()+1, username, password, phone, email, "Admin");
+        this.librarySystem = librarySystem;
+    }
+
+    public User createUser(String username, String email, String phone, String password, String role) {
+        Long userId = librarySystem.getUserCount()+1;
+        User newUser = new User(userId, username, password, phone, email, role);
+        librarySystem.getUserMap().put(userId, newUser);
+        librarySystem.incrementUsers();
+        return newUser;
+    }
+
+    public boolean deleteUser(Long userId) {
+        if (librarySystem.getUserMap().containsKey(userId)) {
+            librarySystem.getUserMap().remove(userId);
+            librarySystem.decrementUsers();
+            return true;
+        }
+        return false;
+    }
     
+    public Boolean updateOwnUsername(Long userId, String newName) {
+        if (this.getId().equals(userId)) {
+            this.setUsername(newName);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean updateOwnPhone(Long userId, String newPhone) {
+        if (this.getId().equals(userId)) {
+            this.setPhone(newPhone);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean updateOwnEmail(Long userId, String newEmail) {
+        if (this.getId().equals(userId)) {
+            this.setEmail(newEmail);
+            return true;
+        }
+        return false;
+    }
+    
+    public Boolean updateUsername(Long userId, String newName, Map<Long, User> userMap) {
+        if (userMap.containsKey(userId)) {
+            User u = userMap.get(userId);
+            u.setUsername(newName);
+            return true;
+        }
+        return false;
+    }
+
+     public Boolean updateUserPhone(Long userId, String newPhone, Map<Long, User> userMap) {
+        if (userMap.containsKey(userId)) {
+            User u = userMap.get(userId);
+            u.setPhone(newPhone);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean updateUserEmail(Long userId, String newEmail, Map<Long, User> userMap) {
+        if (userMap.containsKey(userId)) {
+            User u = userMap.get(userId);
+            u.setEmail(newEmail);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean addBook(String title, String author, int year, String genre, int amount, Map<Long, Book> bookMap) {
+        Long newId = (long) (bookMap.size() + 1);
+        Book book = new Book(newId, title, author, genre, year, amount);
+        bookMap.put(newId, book);
+        return true;
+    }
+
+    public Boolean updateBook(Long bookId, String title, String author, int year, String genre, HashMap<Long, Book> bookMap) {
+        if (bookMap.containsKey(bookId)) {
+            bookMap.get(bookId).updateInfo(title, author, year, genre);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeBook(Long bookId, HashMap<Long, Book> bookMap) {
+        if (bookMap.containsKey(bookId)) {
+            bookMap.remove(bookId);
+            librarySystem.decrementBooks();
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean updateBookStatus(Long bookId, String status, HashMap<Long, Book> bookMap) {
+        if (bookMap.containsKey(bookId)) {
+            bookMap.get(bookId).updateStatus(status);
+            return true;
+        }
+        return false;
+    }
 }
